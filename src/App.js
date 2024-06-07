f02a9bd1ddc07f3110dd5d995971509bed483800
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-06-07 15:36:05 +0200

    set up search form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import logo from './logo.png';
 import './App.css';
+import Dictionary from "./Dictionary";
+
+
 
 function App() {
   return (
     <div className="App">
+      <div className='"container'>
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
-     Hello
+    
       </header>
-      <footer>
+      <main>
+        <Dictionary />
+
+
+      </main>
+      <footer className='App-footer'>
           This project is created by {""}
-          <a href="https://www.linkedin.com/in/kristilerla/">Kristil Erla</a> and is open sourced on {""}
-      <a href="https://github.com/kristilerla/monday-weather-app"> Github</a>
+          <a href="https://www.linkedin.com/in/kristilerla/" target='blanc'>Kristil Erla</a> and is open sourced on {""}
+      <a href="https://github.com/kristilerla/yoda-help" target='blank'> Github</a>
     </footer>
+    </div>
     </div>
   );
 }
