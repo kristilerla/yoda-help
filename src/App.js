72363c9f28649ced1def4b2d9b1a9c0190a773de
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-06-07 10:24:26 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+     Hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
