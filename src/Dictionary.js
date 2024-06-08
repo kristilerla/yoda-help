829ea7723a3c2f3a59226d9bb51b9deb082fd6e1
Kristil Erla Håland <78584348+kristilerla@users.noreply.github.com>
2024-06-08 13:05:37 +0200

    rendered the meanings of the words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
+
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        
+    setResults(response.data[0]);
+    
     }
 
     function search(event){
@@ -27,6 +32,8 @@ return (
 <input type="search" onChange={handleKeywordChange} />
 
     </form>
+   
+    <Results results={results} />
   
     </div>
 );
